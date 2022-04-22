8750444d3e2f64736d4e29b254d0cabb2df2552b
宋宇欣 <kiko@xiaohongshu.com>
2022-04-22 16:25:54 +0800

    update

--- model.py
+++ model.py
@@ -489,10 +489,7 @@ def window_partition(x, win_size, dilation_rate=1):
         windows = x.permute(0,2,1).contiguous().view(-1, C, win_size, win_size) # B' ,C ,Wh ,Ww
         windows = windows.permute(0,2,3,1).contiguous() # B' ,Wh ,Ww ,C
     else:
-        try:
-            x = x.view(B, H // win_size, win_size, W // win_size, win_size, C)
-        except:
-            print (x.size())
+        x = x.view(B, H // win_size, win_size, W // win_size, win_size, C)
         windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, win_size, win_size, C) # B' ,Wh ,Ww ,C
     return windows
 
