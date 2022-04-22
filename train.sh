7e8a21b06a2c7664fac72dd1cf67eedb86a90c96
宋宇欣 <kiko@xiaohongshu.com>
2022-04-22 11:52:09 +0800

    update

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 # Uformer16
 python3 ./train.py --arch Uformer --batch_size 32 --gpu '0,1' \
-    --train_ps 128 --train_dir dataset/GOPRO/train --env 16_0701_1 \
-    --val_dir dataset/GOPRO/test --embed_dim 16 --warmup
+    --train_ps 128 --train_dir ../MIMO-UNet/dataset/GOPRO/train --env 16_0701_1 \
+    --val_dir ../MIMO-UNet/dataset/GOPRO/test --embed_dim 16 --warmup
 
 # Uformer32
 # python3 ./train.py --arch Uformer --batch_size 32 --gpu '0,1' \
