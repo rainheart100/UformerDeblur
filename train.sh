99a514d356c71ebb5d2ce444160869fb9ed93705
宋宇欣 <kiko@xiaohongshu.com>
2022-04-22 18:44:33 +0800

    update

--- train.sh
+++ train.sh
@@ -1,5 +1,4 @@
 # Uformer16
-# all patch_size is 128
 python3 ./train.py --arch Uformer --batch_size 64 --val_interval 1 --gpu '0,1,2,3' \
     --train_ps 128 --train_dir ../MIMO-UNet/dataset/GOPRO/train --env 16_0701_1 \
     --val_dir ../MIMO-UNet/dataset/GOPRO/test --embed_dim 16 --warmup
